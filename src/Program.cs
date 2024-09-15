37fd74dba0b9737248449e6df2309e74393033de
Simonas <simonas.pacauskas@gmail.com>
2024-09-15 18:25:28 +0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -33,12 +33,53 @@ if (command == "decode")
             throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
         }
     }
-    // parse integer.
-    // Integers are encoded as i<number>e. For example, 52 is encoded as i52e and -52 is encoded as i-52e.
     else if (encodedValue[0] == 'i')
     {
         var longValue = long.Parse(encodedValue[1..^1]);
         Console.WriteLine(JsonSerializer.Serialize(longValue));
+    }
+    else if (encodedValue[0] == 'l')
+    {
+        var listValue = new List<object>();
+        var i = 1;
+        while (i < encodedValue.Length - 1)
+        {
+            var element = encodedValue[i];
+            if (char.IsDigit(element))
+            {
+                var colonIndex = encodedValue.IndexOf(':', i);
+                if (colonIndex != -1)
+                {
+                    var strLength = int.Parse(encodedValue[i..colonIndex]);
+                    var strValue = encodedValue.Substring(colonIndex + 1, strLength);
+                    listValue.Add(strValue);
+                    i = colonIndex + 1 + strLength;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
+                }
+            }
+            else if (element == 'i')
+            {
+                var endIndex = encodedValue.IndexOf('e', i);
+                var longValue = long.Parse(encodedValue[i..endIndex]);
+                listValue.Add(longValue);
+                i = endIndex + 1;
+            }
+            else if (element == 'l')
+            {
+                var endIndex = encodedValue.IndexOf('e', i);
+                var listElement = encodedValue[i..(endIndex + 1)];
+                listValue.Add(listElement);
+                i = endIndex + 1;
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
+            }
+        }
+        Console.WriteLine(JsonSerializer.Serialize(listValue));
     }
     else
     {
