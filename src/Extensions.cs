5687b4171c75015c0642acc9da5b4000ef30151b
Simonas <simonas.pacauskas@gmail.com>
2024-10-03 15:28:37 +0300

    codecrafters submit [skip ci]

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using System.Text;
 
 namespace codecrafters_bittorrent;
@@ -19,5 +20,71 @@ public static class Extensions
             (byte)'d' => BencodingType.Dictionary,
             _ => throw new InvalidOperationException("Invalid encoded value: " + Encoding.UTF8.GetString(encodedValue))
         };
+    }
+    
+    public static Dictionary<byte[], object> ToDictionary(this byte[] torrentBytes)
+    {
+        var index = 0;
+        var torrentBytesDictionary = BencodeDecoder.DecodeDictionary(ref torrentBytes, ref index);
+        return torrentBytesDictionary;
+    }
+
+    public static Dictionary<byte[], object> ToInfoDictionary(this Dictionary<byte[], object> torrentDictionary)
+    {
+        var infoDictionary = (Dictionary<byte[], object>)torrentDictionary["info"u8.ToArray()];
+        return infoDictionary;
+    }
+    
+    public static byte[] ToPiecesInBytes(this Dictionary<byte[], object> infoDictionary)
+    {
+        var piecesInBytes = (byte[])infoDictionary["pieces"u8.ToArray()];
+        return piecesInBytes;
+    }
+    
+    public static string ToInfoHash(this Dictionary<byte[], object> infoDictionary)
+    {
+        return BencodeEncoder.EncodeDictionary(infoDictionary).CalculateInfoHash();
+    }
+    
+    public static string ToTrackerUrl(this Dictionary<byte[], object> torrentDictionary)
+    {
+        return Encoding.UTF8.GetString((byte[])torrentDictionary["announce"u8.ToArray()]);
+    }
+    
+    public static string ToName(this Dictionary<byte[], object> infoDictionary)
+    {
+        return Encoding.UTF8.GetString((byte[])infoDictionary["name"u8.ToArray()]);
+    }
+    
+    public static long ToLength(this Dictionary<byte[], object> infoDictionary)
+    {
+        return (long)infoDictionary["length"u8.ToArray()];
+    }
+    
+    public static long ToPieceLength(this Dictionary<byte[], object> infoDictionary)
+    {
+        return (long)infoDictionary["piece length"u8.ToArray()];
+    }
+    
+    public static List<string> ToPieceHashes(this byte[] piecesInBytes)
+    {
+        var pieces = new List<string>();
+        for (var i = 0; i < piecesInBytes.Length; i += 20)
+        {
+            var piece = piecesInBytes[i..(i + 20)];
+            pieces.Add(BitConverter.ToString(piece).Replace("-", "").ToLower());
+        }
+
+        return pieces;
+    }
+
+    private static string CalculateInfoHash(this byte[] bencodedBytes)
+    {
+        var infoHash = SHA1.HashData(bencodedBytes);
+
+        var infoHashString = BitConverter.ToString(infoHash)
+            .Replace("-", "").ToLower();
+
+        return infoHashString;
     }
 }
--- src/Torrent.cs
+++ src/Torrent.cs
@@ -2,4 +2,13 @@ namespace codecrafters_bittorrent;
 
 public class Torrent(byte[] torrentBytes)
 {
+    private Dictionary<byte[], object> TorrentDictionary => torrentBytes.ToDictionary();
+    private Dictionary<byte[], object> InfoDictionary => TorrentDictionary.ToInfoDictionary();
+    private byte[] PiecesInBytes => InfoDictionary.ToPiecesInBytes();
+    public string InfoHash => InfoDictionary.ToInfoHash();
+    public string TrackerUrl => TorrentDictionary.ToTrackerUrl();
+    public string Name => InfoDictionary.ToName();
+    public long Length => InfoDictionary.ToLength();
+    public long PieceLength => InfoDictionary.ToPieceLength();
+    public List<string> PieceHashes => PiecesInBytes.ToPieceHashes();
 }
