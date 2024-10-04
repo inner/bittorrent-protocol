86780c5f50d31b435c5c8b6d24ed429babdd6c3b
Simonas <simonas.pacauskas@gmail.com>
2024-10-04 10:50:28 +0300

    codecrafters submit [skip ci]

--- src/Commands/Handshake.cs
+++ src/Commands/Handshake.cs
@@ -29,20 +29,35 @@ public class Handshake : IBCommand
         
         var handshake = handshakeBytes.ToArray();
         
-        // connect to the peer ip:port and send the handshake, receive the response
         var peerIp = peerToConnect.Split(':')[0];
         var peerPort = int.Parse(peerToConnect.Split(':')[1]);
         var peerEndPoint = new IPEndPoint(IPAddress.Parse(peerIp), peerPort);
         var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         await socket.ConnectAsync(peerEndPoint);
         await socket.SendAsync(handshake, SocketFlags.None);
+        
         // read response
         var response = new byte[handshake.Length];
         await socket.ReceiveAsync(response, SocketFlags.None);
+        
         // close the connection
         socket.Shutdown(SocketShutdown.Both);
         socket.Close();
-        // print peer id from response
-        Console.WriteLine($"Length: {response.Length}");
+        
+        // parse response with the same fields as handshake
+        var responseProtocolStringLength = response[0];
+        var responseProtocolString = Encoding.UTF8.GetString(response, 1, 19);
+        var responseReserved = response[20..28];
+        var responseInfoHash = response[28..48];
+        var responsePeerId = response[48..68];
+        
+        // print all fields
+        Console.WriteLine($"Protocol string length: {responseProtocolStringLength}");
+        Console.WriteLine($"Protocol string: {responseProtocolString}");
+        Console.WriteLine($"Reserved: {Encoding.UTF8.GetString(responseReserved)}");
+        Console.WriteLine($"Info hash: {BitConverter.ToString(responseInfoHash).Replace("-", "").ToLower()}");
+        
+        // print peer id in hexadecimal
+        Console.WriteLine($"Peer ID: {BitConverter.ToString(responsePeerId).Replace("-", "").ToLower()}");
     }
 }
