9a94dc6897852332acad2785b947504681748267
Simonas <simonas.pacauskas@gmail.com>
2024-10-07 17:08:20 +0300

    codecrafters submit [skip ci]

--- src/Connection/PeerConnectionExtensions.cs
+++ src/Connection/PeerConnectionExtensions.cs
@@ -34,7 +34,7 @@ public static class PeerConnectionExtensions
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex);
+            Console.WriteLine(ex.Message);
             throw;
         }
     }
