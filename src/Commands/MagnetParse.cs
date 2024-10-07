be8f5a95a6fcccf409f5a548e33248b658705efe
Simonas <simonas.pacauskas@gmail.com>
2024-10-07 17:55:21 +0300

    codecrafters submit [skip ci]

--- src/Commands/MagnetParse.cs
+++ src/Commands/MagnetParse.cs
@@ -7,14 +7,13 @@ public class MagnetParse : IBCommand
     public Task Execute(string[] args)
     {
         var magnet = args[1];
-        // magnet:?xt=urn:btih:ad42ce8109f54c99613ce38f9b4d87e70f24a165&dn=magnet1.gif&tr=http%3A%2F%2Fbittorrent-test-tracker.codecrafters.io%2Fannounce
         
-        // parse Tracker URL
         var trackerUrl = magnet.Split("&tr=")[1];
         trackerUrl = WebUtility.UrlDecode(trackerUrl);
-        Console.WriteLine($"Tracker URL: {trackerUrl}");
-        // parse Info Hash
+        
         var infoHash = magnet.Split("&dn=")[0].Split(":")[3];
+        
+        Console.WriteLine($"Tracker URL: {trackerUrl}");
         Console.WriteLine($"Info Hash: {infoHash}");
         
         return Task.CompletedTask;
